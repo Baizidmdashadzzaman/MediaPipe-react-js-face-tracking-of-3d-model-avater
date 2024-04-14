55389bf9d084187ac9b1ee748a7c0dee80a760fb
Baizid MD Ashadzzaman <86706204+Baizidmdashadzzaman@users.noreply.github.com>
2024-04-14 18:16:46 +0600

    Create Mediapipe3dModelFaceTracking.jsx

---
+++ Mediapipe3dModelFaceTracking.jsx
@@ -0,0 +1,185 @@
+import './Mediapipe3dModelFaceTrackingCss.css';
+
+import { useEffect, useState } from 'react';
+import { FaceLandmarker, FaceLandmarkerOptions, FilesetResolver } from "@mediapipe/tasks-vision";
+import { Color, Euler, Matrix4 } from 'three';
+import { Canvas, useFrame, useGraph } from '@react-three/fiber';
+import { useGLTF } from '@react-three/drei';
+import { useDropzone } from 'react-dropzone';
+
+let video;
+let faceLandmarker;
+let lastVideoTime = -1;
+let blendshapes = [];
+let rotation;
+let headMesh = [];
+
+const options = {
+  baseOptions: {
+    modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+    delegate: "GPU"
+  },
+  numFaces: 1,
+  runningMode: "VIDEO",
+  outputFaceBlendshapes: true,
+  outputFacialTransformationMatrixes: true,
+};
+
+function Avatar({ url }) {
+  const { scene } = useGLTF(url);
+  const { nodes } = useGraph(scene);
+
+  useEffect(() => {
+    if (nodes.Wolf3D_Head) headMesh.push(nodes.Wolf3D_Head);
+    if (nodes.Wolf3D_Teeth) headMesh.push(nodes.Wolf3D_Teeth);
+    if (nodes.Wolf3D_Beard) headMesh.push(nodes.Wolf3D_Beard);
+    if (nodes.Wolf3D_Avatar) headMesh.push(nodes.Wolf3D_Avatar);
+    if (nodes.Wolf3D_Head_Custom) headMesh.push(nodes.Wolf3D_Head_Custom);
+  }, [nodes, url]);
+
+  useFrame(() => {
+    if (blendshapes.length > 0) {
+      blendshapes.forEach(element => {
+        headMesh.forEach(mesh => {
+          let index = mesh.morphTargetDictionary[element.categoryName];
+          if (index >= 0) {
+            mesh.morphTargetInfluences[index] = element.score;
+          }
+        });
+      });
+
+      nodes.Head.rotation.set(rotation.x, rotation.y, rotation.z);
+      nodes.Neck.rotation.set(rotation.x / 5 + 0.3, rotation.y / 5, rotation.z / 5);
+      nodes.Spine2.rotation.set(rotation.x / 10, rotation.y / 10, rotation.z / 10);
+    }
+  });
+
+  return <primitive object={scene} position={[0, -1.75, 3]} />
+}
+
+function Mediapipe3dModelFaceTracking() {
+
+  const [url, setUrl] = useState("https://models.readyplayer.me/66191e0899c9bc2c58baabd0.glb?morphTargets=ARKit&textureAtlas=1024");
+  const { getRootProps } = useDropzone({
+    onDrop: files => {
+      const file = files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        setUrl(reader.result);
+      }
+      reader.readAsDataURL(file);
+    }
+  });
+
+  const setup = async () => {
+    const filesetResolver = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
+    faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, options);
+
+    video = document.getElementById("video");
+    navigator.mediaDevices.getUserMedia({
+      video: { width: 1280, height: 720 },
+      audio: false,
+    }).then(function (stream) {
+      video.srcObject = stream;
+      video.addEventListener("loadeddata", predict);
+    });
+  }
+
+  const predict = async () => {
+    let nowInMs = Date.now();
+    if (lastVideoTime !== video.currentTime) {
+      lastVideoTime = video.currentTime;
+      const faceLandmarkerResult = faceLandmarker.detectForVideo(video, nowInMs);
+
+      if (faceLandmarkerResult.faceBlendshapes && faceLandmarkerResult.faceBlendshapes.length > 0 && faceLandmarkerResult.faceBlendshapes[0].categories) {
+        blendshapes = faceLandmarkerResult.faceBlendshapes[0].categories;
+
+        const matrix = new Matrix4().fromArray(faceLandmarkerResult.facialTransformationMatrixes[0].data);
+        rotation = new Euler().setFromRotationMatrix(matrix);
+      }
+    }
+
+    window.requestAnimationFrame(predict);
+  }
+
+  const handleOnChange = (event) => {
+    setUrl(`${event.target.value}?morphTargets=ARKit&textureAtlas=1024`);
+  }
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  return (
+    <>
+    
+<main  id="myTabContent" role="main" className="1 lg:flex lg:flex-1 lg:flex-col ">
+
+
+<div >
+
+
+
+<div className="relative ">
+
+
+  <div className="relative overflow-x-clip">
+    <img style={{position: 'absolute', width: 700, height: 700, left: '-15%', top: 0, opacity: '.75'}} src="/static/img/blur.svg" className="animate-pulse-slow" />
+    <div className="relative mx-auto mt-2 max-w-5xl px-6">
+
+
+<div className="mt-16 row">
+
+<div className="col-lg-12 col-sm-12 mb-4" style={{width:'100%'}}>
+    <div className="w-full bg-gray-850 rounded-xl mx-auto px-12 py-8" style={{boxShadow: 'inset 0px -1px 0px rgba(0, 0, 0, 0.5), inset 0px 1px 0px rgba(255, 255, 255, 0.1)',width:'100%'}}>
+    <h1  className="mx-auto max-w-4xl px-16 text-center text-3xl font-semibold leading-tight sm:text-3xl">
+      Mediapipe 3d model face tracking
+    </h1>
+    
+    <center>
+
+
+    <div className="App">
+      
+      {/* <div {...getRootProps({ className: 'dropzone' })}>
+        <p>Drag & drop RPM avatar GLB file here</p>
+      </div>
+      <input className='url' type="text" placeholder="Paste RPM avatar URL" onChange={handleOnChange} /> */}
+      <Canvas style={{ height: 400 }} camera={{ fov: 25 }} shadows>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} color={new Color(1, 1, 0)} intensity={0.5} castShadow />
+        <pointLight position={[-10, 0, 10]} color={new Color(1, 0, 0)} intensity={0.5} castShadow />
+        <pointLight position={[0, 0, 10]} intensity={0.5} castShadow />
+        <Avatar url={url} />
+      </Canvas>
+      <video className='logo' id="video" autoPlay></video>
+      {/* <img className='logo' src="/static/images/ReadyPlayerMe-Avatar.jpeg" /> */}
+    </div>
+
+
+
+    </center>
+
+    
+
+    </div>
+  </div>
+
+</div>
+
+    </div>
+  </div>
+
+</div>
+
+
+</div>
+
+</main>
+
+    
+    </>
+  )
+}
+
+export default Mediapipe3dModelFaceTracking
